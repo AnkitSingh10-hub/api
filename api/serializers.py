08edaeb58eef9466ee845a14308679d1bcac1657
ankit singh <ankitsinghtheweeknd@gmail.com>
2023-05-28 22:00:44 +0545

    second commit

--- api/serializers.py
+++ api/serializers.py
@@ -1,29 +1,15 @@
 from .models import *
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
 
-class EmployeeSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=30)
-    email = serializers.EmailField()
-    password = serializers.CharField(max_length=30)
-    phone = serializers.CharField(max_length=10)
-
-    def create(self, validated_data):
-        print("create method called..")
-        return Employee.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.email = validated_data.get('email', instance.email)
-        instance.password = validated_data.get('password', instance.password)
-        instance.phone = validated_data.get('phone', instance.phone)
-        instance.save()
-        return instance
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employee
+        fields = '__all__'
 
 
-class UserSerializer(serializers.Serializer):
-    username = serializers.CharField(max_length=30)
-    email = serializers.EmailField()
-    password = serializers.CharField(max_length=30)
-    first_name = serializers.CharField(max_length=30)
-    last_name = serializers.CharField(max_length=30)
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
